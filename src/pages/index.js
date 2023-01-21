780f64256ec4011b0a4e6f6bae6027b5c47e10ce
Gopal Kaul <gopalkaul5@gmail.com>
2023-01-21 18:17:20 +0530

    More changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useEffect, useRef } from "react";
 export default function Home() {
-  const main = useRef(null);
-  const co = useRef(null);
   // useEffect(() => {
   //   const scroll = function (e) {
   //     if (e.deltaY > 0) main.current.scrollIntoView({ behaviour: "smooth" });
@@ -26,52 +24,27 @@ export default function Home() {
       </Head>
       <main>
         <Fade>
-          <div
-            className={`h-screen bg-[url('/carousel/2.jpg')] bg-cover bg-center bg-no-repeat overflow-scroll scroll-smooth`}
-            onWheel={(e) => {
-              if (e.deltaY > 0) main.current.scrollIntoView();
-              else co.current.scrollIntoView();
-            }}
-          >
-            {/* <img
-                  src={`/carousel/${ele}.jpg`}
-                  className="min-h-[100vh]"
-                /> */}
-            {/* <div className="absolute left-[10%] w-[80%] md:w-[40%] top-[5%] bg-white px-[5%] pt-[3%] pb-[4%] items-start text-start font-opensans">
-                  <h2 className="text-[2vw] font-bold">
-                    Hotel Curated Package
+          <div className="p-[1%] h-screen w-full overflow-y-scroll bg-[#1a1a1a]">
+            <div
+              className={`h-full bg-[url('/carousel/2.jpg')] bg-cover bg-center bg-no-repeat overflow-scroll snap-start snap-y snap-mandatory snap-always`}
+            >
+              <div className="sticky bottom-0">
+                <div className="h-screen snap-start"></div>
+                <div className="text-white w-full px-[5%] pt-[2%] pb-[2%] items-start text-start font-opensans bg-transparent/60 snap-start">
+                  <h2 className="font-bold text-3xl pb-6 font-graphik">
+                    Sample Title
                   </h2>
-                  <p className="text-[1.4vw] leading-normal mb-10 py-[5%]">
-                    Our new custom design package is timeless and stylish,
-                    inspired by textures of nature. Shop this package on
-                    Marketplace.
+                  <p className="text-base">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Architecto, tenetur ea. Rerum animi id quod, inventore quam
+                    facere laboriosam placeat itaque. Sequi necessitatibus
+                    sapiente nulla debitis suscipit sed dolores. Consectetur?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Aliquam error saepe ut reprehenderit libero numquam deserunt
+                    sed facere aliquid! Odit, recusandae praesentium sint iusto
+                    iure molestias soluta autem ipsa amet.
                   </p>
-                  <a
-                    href="#"
-                    className="bg-brown rounded-md text-white px-10 py-5 text-[1.3vw] font-bold transition-all duration-200"
-                  >
-                    Read More
-                  </a>
-                </div> */}
-            <div className="sticky bottom-0">
-              <div ref={co} className="h-screen"></div>
-              <div
-                className="text-white w-full px-[5%] pt-[2%] pb-[2%] items-start text-start font-opensans bg-transparent/60"
-                ref={main}
-              >
-                <h2 className="font-bold text-3xl pb-6 font-graphik">
-                  Sample Title
-                </h2>
-                <p className="text-base">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Architecto, tenetur ea. Rerum animi id quod, inventore quam
-                  facere laboriosam placeat itaque. Sequi necessitatibus
-                  sapiente nulla debitis suscipit sed dolores. Consectetur?
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Aliquam error saepe ut reprehenderit libero numquam deserunt
-                  sed facere aliquid! Odit, recusandae praesentium sint iusto
-                  iure molestias soluta autem ipsa amet.
-                </p>
+                </div>
               </div>
             </div>
           </div>
