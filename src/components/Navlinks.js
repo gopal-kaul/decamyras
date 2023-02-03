29fa535a804d10b945844bfbb5d2f909f179efd3
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-03 23:23:25 +0530

    Create new navlinks to hold navlinks

---
+++ src/components/Navlinks.js
@@ -0,0 +1,10 @@
+import ActiveLink from "./ActiveLink";
+
+export default function Navlinks() {
+  return (
+    <>
+      <ActiveLink href="/">home</ActiveLink>
+      <ActiveLink href="/contactus">contact us</ActiveLink>
+    </>
+  );
+}
