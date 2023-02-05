98ac9b0f09ac216716c809222d09b12d954c75bf
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-06 00:31:36 +0530

    Work on contact page

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,4 +1,5 @@
 import { MdOutlineLocalPhone, MdOutlineEmail } from "react-icons/md";
+import Select from "react-select/";
 import Head from "next/head";
 export default function Home() {
   return (
@@ -9,52 +10,118 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="flex justify-center items-center flex-col min-h-full bg-white gap-y-[10%]">
-        <div className="grid grid-cols-1 md:grid-cols-2 w-full place-content-evenly place-items-center py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-y-4 place-content-evenly place-items-center py-10">
           <a
-            className="flex flex-row items-center gap-2"
+            className="flex flex-row items-center gap-4 border-4 border-gold shadow-lg px-4 py-2 rounded-lg"
             href="tel:+911234123412"
           >
             <MdOutlineLocalPhone />
             +91-1234567890
           </a>
           <a
-            className="flex flex-row items-center gap-2"
+            className="flex flex-row items-center gap-4 border-4 border-gold shadow-lg px-4 py-2 rounded-lg"
             href="mailto:info@decamyras.com"
           >
             <MdOutlineEmail />
             info@decamyras.com
           </a>
         </div>
-        <div className="flex flex-col md:flex-row w-full gap-y-5 md:gap-0 grow pb-4 md:pb-0">
+        <div className="flex flex-col md:flex-row w-full gap-y-5 gap-x-20 grow pb-4 md:pb-20 px-4 md:px-20">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.4843388213676!2d78.4902006!3d17.436517399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9a1bb1955555%3A0x7ff18a28fa08c210!2sGovdan%20Group!5e0!3m2!1sen!2sin!4v1675441974338!5m2!1sen!2sin"
             allowfullscreen=""
-            className="w-full md:w-1/2 min-h-[50vh] md:min-h-full"
+            className="w-full md:w-[40%] min-h-[40vh] md:min-h-full"
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
           <form
             action="#"
             method="post"
-            className="px-4 flex flex-col gap-3 w-1/2"
+            className="px-4 py-8 md:p-20 flex flex-col justify-between gap-3 md:w-[60%] bg-gray rounded-lg"
           >
-            <label className="font-bold">Name</label>
-            <input
-              placeholder="John Doe"
-              type="text"
-              name="name"
-              className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
-            />
-            <label className="font-bold">Email</label>
-            <input
-              placeholder="johndoe@gmail.com"
-              type="email"
-              name="email"
-              className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
-            />
-            <button type="submit" className="uppercase bg-[#1a1a1a] text-gold transition-colors duration-300 ease-in-out hover:bg-gold hover:text-[#1a1a1a] w-fit px-10 font-bold py-3">
-              Submit
-            </button>
+            <div className="flex flex-col md:flex-row gap-y-3 gap-x-10">
+              <div className="flex flex-col gap-3 w-full md:w-auto">
+                <div className="flex flex-col gap-3">
+                  <label className="font-bold text-white">Name</label>
+                  <input
+                    placeholder="John Doe"
+                    type="text"
+                    name="name"
+                    className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <label className="font-bold text-white">Phone Number</label>
+                  <input
+                    placeholder="+91-1234567890"
+                    type="text"
+                    name="phone"
+                    className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <label className="font-bold text-white">Email Address</label>
+                  <input
+                    placeholder="johndoe@gmail.com"
+                    type="email"
+                    name="email"
+                    className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-3 w-full">
+                <label className="font-bold text-white">
+                  Additional Information
+                </label>
+                <textarea
+                  name="info"
+                  className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-full focus:rounded-sm"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <label className="font-bold text-white">
+                Interested Products
+              </label>
+              <Select
+                isMulti
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#F0DC82",
+                    primary50: "#F0DC82",
+                    primary: "#F0DC82",
+                  },
+                })}
+                className="w-full focus:outline-none border-2 focus:border-4 border-gold px-1 py-1 focus:rounded-sm"
+                options={[
+                  { value: "Linen", label: "Linen" },
+                  { value: "Bedsheets", label: "Bedsheets" },
+                  { value: "Placeholder", label: "Another placeholder" },
+                ]}
+              />
+              {/* <select
+                name="products"
+                multiple
+                className="focus:outline-none ring-2 focus:ring-4 ring-gold px-1 py-1 w-fit focus:rounded-sm"
+              >
+                <option>Linen</option>
+                <option>Bedsheets</option>
+                <option>Another Option</option>
+              </select> */}
+            </div>
+            <div className="grid place-items-center">
+              <button
+                type="submit"
+                className="uppercase bg-gold text-gray transition-[padding] duration-300 ease-in-out w-fit px-10 hover:px-14 font-bold py-2"
+              >
+                Submit
+              </button>
+            </div>
           </form>
         </div>
       </div>
