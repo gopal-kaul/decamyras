109d195fd257787c60ca5b7850890676791a04c4
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-06 20:39:25 +0530

    Work on products page

---
+++ src/pages/products.js
@@ -0,0 +1,104 @@
+import Head from "next/head";
+export default function Products() {
+  return (
+    <>
+      <Head>
+        <title>Contact Us | Decamyra&apos;s</title>
+        <meta name="description" content="Decamyra's" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className="bg-white min-h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 place-items-center px-4 md:px-20 gap-x-10 gap-y-20 py-10">
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+        <div className="relative shadow-xl rounded-3xl">
+          <img
+            src="/products/product1.jpg"
+            className="object-cover rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">Towels</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
