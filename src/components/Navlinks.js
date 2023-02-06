ce5f7c5ca12840b8c638f9091b382c4bc05cb153
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-06 20:40:47 +0530

    Add products to navbar

--- src/components/Navlinks.js
+++ src/components/Navlinks.js
@@ -4,6 +4,7 @@ export default function Navlinks() {
   return (
     <>
       <ActiveLink href="/">home</ActiveLink>
+      <ActiveLink href="/products">products</ActiveLink>
       <ActiveLink href="/contactus">contact us</ActiveLink>
     </>
   );
