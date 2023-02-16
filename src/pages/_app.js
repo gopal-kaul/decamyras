68f470cb68bc719583896d42c36354fc3eea72d9
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-16 19:07:26 +0530

    Replace fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,13 @@
 import "@/styles/globals.css";
 import localFont from "@next/font/local";
 import Navbar from "@/components/Navbar";
-import { Open_Sans } from "@next/font/google";
+import { Quicksand } from "@next/font/google";
 import Footer from "@/components/Footer";
 import { useEffect, useRef } from "react";
 const artifexCF = localFont({
   src: "../fonts/ArtifexCF/ArtifexCF-Regular.woff2",
   variable: "--font-artifexCF",
 });
-const opensans = Open_Sans({ subsets: ["latin"], variable: "--font-opensans" });
 const utsaah = localFont({
   src: [
     {
@@ -34,84 +33,9 @@ const utsaah = localFont({
   ],
   variable: "--font-utsaah",
 });
-const graphik = localFont({
-  src: [
-    {
-      path: "../fonts/Graphik/GraphikBlack.otf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      weight: "800",
-      style: "italic",
-      path: "../fonts/Graphik/GraphikBlackItalic.otf",
-    },
-    {
-      weight: "700",
-      path: "../fonts/Graphik/GraphikBold.otf",
-    },
-    {
-      weight: "700",
-      style: "italic",
-      path: "../fonts/Graphik/GraphikBoldItalic.otf",
-    },
-    {
-      path: "../fonts/Graphik/GraphikExtralight.otf",
-      weight: "200",
-    },
-    {
-      weight: "200",
-      style: "italic",
-      path: "../fonts/Graphik/GraphikExtralightItalic.otf",
-    },
-    {
-      weight: "300",
-      path: "../fonts/Graphik/GraphikLight.otf",
-    },
-    {
-      path: "../fonts/Graphik/GraphikLightItalic.otf",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Graphik/GraphikMedium.otf",
-      style: "600",
-    },
-    {
-      style: "italic",
-      weight: "600",
-      path: "../fonts/Graphik/GraphikMediumItalic.otf",
-    },
-    {
-      path: "../fonts/Graphik/GraphikRegular.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Graphik/GraphikRegularItalic.otf",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Graphik/GraphikSemiboldItalic.otf",
-      style: "italic",
-      weight: "600",
-    },
-    {
-      weight: "600",
-      path: "../fonts/Graphik/GraphikSemibold.otf",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Graphik/GraphikThin.otf",
-      weight: "100",
-    },
-    {
-      weight: "100",
-      style: "italic",
-      path: "../fonts/Graphik/GraphikThinItalic.otf",
-    },
-  ],
-  variable: "--font-graphik",
+const quicksand = Quicksand({
+  variable: "--font-quicksand",
+  subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }) {
@@ -142,10 +66,13 @@ export default function App({ Component, pageProps }) {
   }, []);
   return (
     <div
-      className={` ${utsaah.variable} ${artifexCF.variable} ${graphik.variable} ${opensans.variable} font-sans scroll-smooth w-screen h-screen overflow-hidden bg-[#1a1a1a]`}
+      className={`${artifexCF.variable} ${utsaah.variable} ${quicksand.variable} font-sans scroll-smooth w-screen h-screen overflow-hidden bg-[#1a1a1a]`}
     >
       <Navbar />
-      <main ref={ref} className={`overflow-y-scroll w-full px-4`}>
+      <main
+        ref={ref}
+        className={`overflow-y-scroll w-full px-4 font-quicksand`}
+      >
         <Component {...pageProps} />
       </main>
     </div>
