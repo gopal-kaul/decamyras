a8703e6ef76f4d3ebeb669aa14eada5fe292fef1
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-25 01:19:46 +0530

    Fix active link

--- src/components/ActiveLink.js
+++ src/components/ActiveLink.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-export default function ActiveLink({ href, children, className, ...props }) {
+export default function ActiveLink({ href, children, className }) {
   const router = useRouter();
   return (
     <Link
       href={href}
-      className={`${className} ${router.pathname === href ? "text-gold font-semibold" : "text-[#a89268]"} hover:text-gold hover:scale-105 transition-transform duration-200 ease-in-out`}
+      className={`${className} ${
+        router.pathname.split("/")[1] === href.substring(1)
+          ? "text-gold font-semibold"
+          : "text-[#a89268]"
+      } hover:text-gold hover:scale-105 transition-transform duration-200 ease-in-out`}
     >
       {children}
     </Link>
