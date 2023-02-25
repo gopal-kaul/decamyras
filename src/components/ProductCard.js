0e142a33e15937202975e92e676265d4705e76d6
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-25 21:52:21 +0530

    Create Product into separate component

---
+++ src/components/ProductCard.js
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+
+export default function ProductCard({ product }) {
+  const [flipped, setFlipped] = useState(false);
+  return (
+    <button
+      onMouseEnter={(e) => {
+        e.preventDefault();
+        setFlipped((old) => !old);
+      }}
+      onMouseLeave={(e) => {
+        e.preventDefault();
+        setFlipped((old) => !old);
+      }}
+      onClick={(e) => {
+        e.preventDefault();
+	if(e.target===e.currentTarget)
+        setFlipped((old) => !old);
+      }}
+      className="shadow-xl rounded-3xl w-full sm:w-1/3 md:w-1/4 lg:w-1/6"
+    >
+      <ReactCardFlip isFlipped={flipped}>
+        <div className="relative">
+          <img
+            src={`/products/${product.coverImage}`}
+            className="object-cover z-10 rounded-3xl"
+          />
+          <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
+            <h2 className="font-bold py-1 text-xl">{product.name}</h2>
+          </div>
+        </div>
+        <div className="bg-white w-full h-full rounded-3xl border border-gold">
+          <div className="relative">
+            <img
+              src={`/products/${product.coverImage}`}
+              className="object-cover z-10 rounded-3xl opacity-0"
+            />
+
+            <div className="absolute top-0 left-0 py-[10%] px-[10%] flex flex-col h-full justify-between">
+              <div className="">
+                <h2 className="font-bold text-xl text-center pb-[5%]">
+                  {product.name}
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem officiis culpa porro facilis, voluptas, ea nihil
+                  ad, tempore totam corporis omnis expedita deleniti enim dolor?
+                  Sint nulla molestiae at! Eveniet?
+                </p>
+              </div>
+              <div className="grid place-items-center">
+                <Link
+                  className="uppercase bg-gold text-gray transition-[padding] duration-300 ease-in-out w-fit px-10 hover:px-14 font-bold py-2"
+                  href={`/products/${product.name
+                    .replace(" ", "-")
+                    .toLowerCase()}`}
+                >
+                  Know More
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ReactCardFlip>
+    </button>
+  );
+}
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import data from "@/data.json";
 import Link from "next/link";
+import ProductCard from "@/components/ProductCard";
 export async function getStaticProps() {
   return {
     props: {
@@ -16,32 +17,9 @@ export default function Products({ products }) {
         <meta name="description" content="Decamyra's" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="bg-bgWhite min-h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 place-items-center px-4 md:px-20 gap-x-10 gap-y-20 py-10 rounded-lg">
+      <div className="bg-bgWhite min-h-full flex flex-row flex-wrap px-4 md:px-20 gap-x-10 gap-y-20 py-10 rounded-lg justify-center">
         {products.map((product) => (
-          <Link
-            className="relative shadow-xl rounded-3xl hover:rotate-y-180 transition-all transform-style-3d duration-500 perspective-1000"
-            key={product.name}
-            href={`/products/${product.name.replace(" ", "-").toLowerCase()}`}
-          >
-            <div className="relative backface-hidden">
-              <img
-                src={`/products/${product.coverImage}`}
-                className="object-cover z-10 rounded-3xl"
-              />
-              <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
-                <h2 className="font-bold py-1 text-xl">{product.name}</h2>
-              </div>
-            </div>
-            <div className="absolute top-0 left-0 w-full h-full rotate-y-180 bg-white px-[10%] py-[10%] backface-hidden rounded-3xl shadow-xl">
-              <h2 className="font-bold text-xl text-center pb-[5%]">{product.name}</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem officiis culpa porro facilis, voluptas, ea nihil ad,
-                tempore totam corporis omnis expedita deleniti enim dolor? Sint
-                nulla molestiae at! Eveniet?
-              </p>
-            </div>
-          </Link>
+          <ProductCard key={product.name} product={product} />
         ))}
       </div>
     </>
