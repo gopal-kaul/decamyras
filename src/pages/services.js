917542a59876d09e6093abde52d4eb5e437e3f0d
Gopal Kaul <gopalkaul5@gmail.com>
2023-02-26 20:27:11 +0530

    Fix mobile view for services

--- src/pages/services.js
+++ src/pages/services.js
@@ -7,8 +7,8 @@ export default function Services() {
         <meta name="description" content="Decamyra's" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="grid place-items-center min-h-full max-h-full lg:overflow-hidden bg-bgWhite px-4 md:px-20 gap-y-5 md:gap-0 rounded-lg">
-        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-36 hover:max-h-96">
+      <div className="grid place-items-center min-h-full max-h-full lg:overflow-hidden bg-bgWhite px-4 md:px-20 gap-y-5 md:gap-0 rounded-lg overflow-hidden">
+        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-48 md:max-h-36 hover:max-h-96">
           <h2 className="font-bold text-xl md:text-2xl">Redesigning</h2>
           <hr />
           <p className="text-sm md:text-base">
@@ -24,15 +24,9 @@ export default function Services() {
               sed facilis similique voluptatem laudantium pariatur at, corporis
               reiciendis repellendus distinctio ad suscipit?
             </p>
-            <p className="text-sm md:text-base opacity-0 group-hover:opacity-100 duration-500 ease-in-out transition-all">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              saepe rem veniam, perferendis quae, repudiandae recusandae magni
-              sed facilis similique voluptatem laudantium pariatur at, corporis
-              reiciendis repellendus distinctio ad suscipit?
-            </p>
           </div>
         </div>
-        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-36 hover:max-h-96">
+        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-48 md:max-h-36 hover:max-h-96">
           <h2 className="font-bold text-xl md:text-2xl">Refurbishment</h2>
           <hr />
           <p className="text-sm md:text-base">
@@ -48,15 +42,10 @@ export default function Services() {
               sed facilis similique voluptatem laudantium pariatur at, corporis
               reiciendis repellendus distinctio ad suscipit?
             </p>
-            <p className="text-sm md:text-base opacity-0 group-hover:opacity-100 duration-500 ease-in-out transition-all">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              saepe rem veniam, perferendis quae, repudiandae recusandae magni
-              sed facilis similique voluptatem laudantium pariatur at, corporis
-              reiciendis repellendus distinctio ad suscipit?
-            </p>
+
           </div>
         </div>
-        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-36 hover:max-h-96">
+        <div className="rounded-2xl w-full bg-white shadow-2xl px-5 py-4 hover:scale-105 transition-all duration-500 hover:border-4 border-gold ease-in-out group max-h-48 md:max-h-36 hover:max-h-96">
           <h2 className="font-bold text-xl md:text-2xl">Replenishment</h2>
           <hr />
           <p className="text-sm md:text-base">
@@ -66,12 +55,6 @@ export default function Services() {
             reiciendis repellendus distinctio ad suscipit?
           </p>
           <div className="invisible group-hover:visible duration-200 transition-all">
-            <p className="text-sm md:text-base opacity-0 group-hover:opacity-100 duration-500 ease-in-out transition-all">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              saepe rem veniam, perferendis quae, repudiandae recusandae magni
-              sed facilis similique voluptatem laudantium pariatur at, corporis
-              reiciendis repellendus distinctio ad suscipit?
-            </p>
             <p className="text-sm md:text-base opacity-0 group-hover:opacity-100 duration-500 ease-in-out transition-all">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
               saepe rem veniam, perferendis quae, repudiandae recusandae magni
