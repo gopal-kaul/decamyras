1df72bba381e66eb991d28eb1e4476b1025911ff
Gopal Kaul <gopalkaul5@gmail.com>
2023-03-04 22:04:55 +0530

    Change homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,17 +27,21 @@ export default function Home() {
             <h2 className="font-artifexCF font-bold text-4xl text-center lg:text-6xl tracking-widest leading-tight pt-2 md:pt-2 text-gold">
               Decamyra&apos;s
             </h2>
-            <p className="font-utsaah text-xl text-center lg:text-5xl tracking-wide text-white pt-2 md:pt-10 pb-5 font-semibold">
+            <p className="font-utsaah text-xl text-center lg:text-5xl tracking-wide text-white pt-2 md:pt-10 pb-10 font-semibold">
               A Luxury Linen and Hospitality Solution
             </p>
             <Link
               href="/products"
               className="bg-gold px-12 text-gray font-bold uppercase py-2 rounded-lg group relative hover:pl-8 hover:pr-16 transition-[padding]"
             >
-              Explore
-                <svg xmlns="http://www.w3.org/2000/svg" className="absolute opacity-0 transition-opacity ease-in-out right-5 top-0 animate-bounce-x-4 group-hover:opacity-100 h-full py-2" viewBox="0 0 448 512">
-                  <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
-                </svg>
+              Our Products
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute opacity-0 transition-opacity ease-in-out right-5 top-0 animate-bounce-x-4 group-hover:opacity-100 h-full py-2"
+                viewBox="0 0 448 512"
+              >
+                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+              </svg>
             </Link>
           </div>
         </div>
