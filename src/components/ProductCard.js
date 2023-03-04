61d88a0f8d911803047d42d9fce01086b7a66c8c
Gopal Kaul <gopalkaul5@gmail.com>
2023-03-05 01:13:30 +0530

    Adjust height of product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -18,25 +18,20 @@ export default function ProductCard({ product }) {
         e.preventDefault();
         if (e.target === e.currentTarget) setFlipped((old) => !old);
       }}
-      className="shadow-xl rounded-3xl w-full sm:w-1/3 md:w-1/4 lg:w-1/6 cursor-pointer"
+      className="shadow-xl rounded-3xl w-full sm:w-1/3 lg:w-1/4 h-[35vh] cursor-pointer"
     >
       <ReactCardFlip isFlipped={flipped}>
-        <div className="relative h-[40vh]">
+        <div className="relative h-[35vh]">
           <img
             src={`/products/${product.coverImage}`}
-            className="object-cover w-full h-[40vh] z-10 rounded-3xl"
+            className="object-cover w-full h-[35vh] z-10 rounded-3xl"
           />
           <div className="absolute bottom-0 left-0 w-full text-center rounded-3xl bg-opacity-50 bg-cardWhite">
             <h2 className="font-bold py-1 text-xl">{product.name}</h2>
           </div>
         </div>
-        <div className="bg-white w-full h-[40vh] rounded-3xl border border-gold">
-          <div className="relative">
-            <img
-              src={`/products/${product.coverImage}`}
-              className="object-cover z-10 rounded-3xl h-[40vh] opacity-0"
-            />
-
+        <div className="bg-white w-full h-[35vh] rounded-3xl border border-gold">
+          <div className="relative h-full">
             <div className="absolute top-0 left-0 py-[10%] px-[10%] flex flex-col h-full justify-between">
               <div className="">
                 <h2 className="font-bold text-xl text-center pb-[5%]">
@@ -49,9 +44,9 @@ export default function ProductCard({ product }) {
               </div>
               <div className="grid place-items-center">
                 <Link
-                  className="uppercase bg-gold text-gray transition-[padding] duration-300 ease-in-out w-fit px-10 hover:px-14 font-bold py-2 rounded-md"
+                  className="uppercase bg-gold text-gray transition-[padding] duration-350 ease-in-out w-fit px-10 hover:px-14 font-bold py-2 rounded-md"
                   href={`/products/${product.name
-                    .replace(" ", "-")
+                    .replaceAll(" ", "-")
                     .toLowerCase()}`}
                 >
                   Know More
