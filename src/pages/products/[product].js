ca7f4f6e9b279902c850f42af24ccba21a613b7f
Gopal Kaul <gopalkaul5@gmail.com>
2023-03-05 18:51:28 +0530

    Change product subpage

--- src/pages/products/[product].js
+++ src/pages/products/[product].js
@@ -36,35 +36,39 @@ export default function ProductDetail({ product }) {
         <meta name="description" content="Decamyra's" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="bg-bgWhite min-h-full px-4 md:px-20 flex flex-col md:flex-row gap-x-10 rounded-lg">
-        <div className="bg-[#cccccc]/20 w-full md:w-[15%] min-h-full flex flex-col justify-center items-center gap-y-4 text-xl font-bold my-auto py-10 rounded-lg">
-          {data.products.map((prod) => (
-            <Link
-              key={prod.name}
-              href={`/products/${prod.name.replaceAll(" ", "-").toLowerCase()}`}
-              className={`rounded-md shadow-2xl px-8 py-2 text-center bg-white ease-in-out transition-transform hover:scale-[1.2] w-fit mx-[10%] ${
-                prod.name.replaceAll(" ", "-").toLowerCase() ===
-                product.name.replaceAll(" ", "-").toLowerCase()
-                  ? "border-4 border-gold"
-                  : ""
-              }`}
-            >
-              {prod.name}
-            </Link>
-          ))}
+      <div className="bg-bgWhite min-h-full flex flex-col gap-x-10 rounded-lg">
+        <div className="bg-gold/40 w-full flex flex-row justify-center items-center text-sm md:text-xl font-bold  rounded-lg mx-auto gap-x-5">
+          <div className="overflow-auto gap-x-5 inline-flex py-5 px-5 snap-x snap-proximity scroll-pl-5">
+            {data.products.map((prod) => (
+              <Link
+                key={prod.name}
+                href={`/products/${prod.name
+                  .replaceAll(" ", "-")
+                  .toLowerCase()}`}
+                className={`rounded-md shadow-2xl px-8 py-2 text-center grid items-center bg-white ease-in-out transition-transform hover:scale-[1.2] snap-start  ${
+                  prod.name.replaceAll(" ", "-").toLowerCase() ===
+                  product.name.replaceAll(" ", "-").toLowerCase()
+                    ? "border-4 border-gold"
+                    : ""
+                }`}
+              >
+                {prod.name}
+              </Link>
+            ))}
+          </div>
         </div>
-        <div className="bg-white w-full md:w-[85%] min-h-full py-5">
+        <div className="bg-white w-full min-h-full py-5 px-4 md:px-20">
           <h2 className="font-bold text-center text-6xl font-utsaah">
             {product.name}
           </h2>
-          <div className="flex flex-col justify-between min-h-[93%] py-[5%]">
-            <div className="flex flex-col md:flex-row gap-x-10">
+          <div className="flex flex-col gap-y-5 justify-between min-h-[70%] py-[5%]">
+            <div className="flex flex-col justify-between md:flex-row gap-x-10">
               <Carousel
                 showThumbs={false}
                 autoPlay
                 infiniteLoop
                 showStatus={false}
-                className="md:w-1/2"
+                className="md:w-[40%]"
               >
                 <div className="">
                   <img
@@ -79,8 +83,10 @@ export default function ProductDetail({ product }) {
                   />
                 </div>
               </Carousel>
-              <div className="md:w-1/2 min-h-full bg-white shadow-2xl rounded-xl">
-                <h3 className="text-center text-3xl font-bold pt-5">All Products</h3>
+              <div className="md:w-[40%] min-h-full bg-white shadow-2xl rounded-xl">
+                <h3 className="text-center text-3xl font-bold pt-5">
+                  All Products
+                </h3>
                 <div className="grid grid-cols-3 gap-y-10 py-8 text-xl place-items-center">
                   <p>Item</p>
                   <p>Item</p>
